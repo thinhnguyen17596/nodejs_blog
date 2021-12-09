0aaa9852f8121ebd1b31343678f968895b9a230e
thinhnguyen <thinhnguyen17596@gmail.com>
2021-12-09 11:06:39 +0700

    add bootstrap

---
+++ blog/src/index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const morgan = require('morgan')
+const path = require('path')
+const { engine } = require('express-handlebars');
+
+
+// Using get static file
+app.use(express.static(path.join(__dirname, 'public')));
+
+// HTTP logger
+app.use(morgan('combined'))
+
+
+// Template engine
+app.engine('.hbs', engine({ extname: '.hbs'
+
+}));
+app.set('view engine', '.hbs');
+
+// Set path
+app.set('views', path.join(__dirname, 'resources/views'))
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
