68e3f4b8c068887a3a6540cce831b0fd270415ae
thinhnguyen <thinhnguyen17596@gmail.com>
2021-12-09 16:39:03 +0700

    add more file

--- blog/src/index.js
+++ blog/src/index.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const morgan = require('morgan')
-const path = require('path')
+const express = require('express');
+const app = express();
+const port = 3000;
+const morgan = require('morgan');
+const path = require('path');
 const { engine } = require('express-handlebars');
 
 
@@ -10,21 +10,26 @@ const { engine } = require('express-handlebars');
 app.use(express.static(path.join(__dirname, 'public')));
 
 // HTTP logger
-app.use(morgan('combined'))
+app.use(morgan('combined'));
 
 
 // Template engine
-app.engine('.hbs', engine({ extname: '.hbs'
-
-}));
+app.engine('.hbs', engine({ extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
 // Set path
 app.set('views', path.join(__dirname, 'resources/views'))
 
 app.get('/', (req, res) => {
-  res.render('home')
-})
+  res.render('home');
+});
+
+app.get('/search', (req, res) => {
+  res.render('search');
+});
+app.get('/news', (req, res) => {
+  res.render('news');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
